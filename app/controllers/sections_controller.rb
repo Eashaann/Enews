18412ab7212ffa5e7d2d014eb7239fc300ba6816
Eashaann <donaeasha25@gmail.com>
2017-05-17 15:12:24 +0400

    Sengrid pluggin added

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -29,7 +29,10 @@ class SectionsController < ApplicationController
   end
 
   def thank_you
-
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 
 end
